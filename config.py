7e152d658f1c038bdf4c2fcc96c526b85d5a2982
moll3 <100882695+moll3@users.noreply.github.com>
2023-02-22 09:28:20 +0800

    Update config.py

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ DEFAULT_HEADERS = {
 """
 调试用 COOKIE
 """
-TEST_COOKIE = ''
+TEST_COOKIE = '__ckguid=8rX6Blb46IB1IB727FVjLR97; device_id=213070643316770289008475355761091e7f2d73d0aee84de9960c3557; homepage_sug=a; r_sort_type=score; sajssdk_2015_cross_new_user=1; Hm_lvt_9b7ac3d38f30fe89ff0b8a0546904e58=1677028903; sensorsdata2015jssdkcross={"distinct_id":"18676b646d93c6-08db083d59a0c-26021051-2073600-18676b646daec4","first_id":"","props":{"$latest_traffic_source_type":"直接流量","$latest_search_keyword":"未取到值_直接打开","$latest_referrer":""},"$device_id":"18676b646d93c6-08db083d59a0c-26021051-2073600-18676b646daec4"}; __jsluid_s=1674d8d582efe6c3866400274983080d; sess=BA-gPPLIBrwYt2TQMwdCVTpZjDTzhzLrE8cB+aUzu3d+o+yrktYzPxaAAwtdusWKGnJoEXat32H5P2Jqywm711CyQIjWj5TH6vyMLGJYoMbC1oiBQ3CJ3/ZB0v3; user=user:2879795873|2879795873; smzdm_id=2879795873; _zdmA.uid=ZDMA.PxiQyLolc.1677028926.2419200; Hm_lpvt_9b7ac3d38f30fe89ff0b8a0546904e58=1677028926'
 
 
 """
